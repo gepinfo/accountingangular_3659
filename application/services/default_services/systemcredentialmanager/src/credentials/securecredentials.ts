e32bc1e66e2776a8733e02799e4c0a32014999aa
gepinfo <info@geppettosoftware.com>
2023-03-27 07:59:10 +0000

    code commited from geppetto!

---
+++ application/services/default_services/systemcredentialmanager/src/credentials/securecredentials.ts
@@ -0,0 +1,4 @@
+export let credentials:any = {
+    DB_AUTH_accountingangular_3659: 'mongodb://admin:password@',
+    DB_COLL_accountingangular_3659: ':27017/accountingangular_3659?authSource=admin'
+}
